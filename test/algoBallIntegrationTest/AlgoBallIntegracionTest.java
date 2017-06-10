d4f2ba211030a5bf692394cde25ac65d0210d39f
FrancoLiberali <franco.liberali@gmail.com>
2017-06-10 13:29:00 -0300

    sacando tildes y ennies que rompen travis(si,otra vez)

--- test/algoBallIntegrationTest/AlgoBallIntegracionTest.java
+++ test/algoBallIntegrationTest/AlgoBallIntegracionTest.java
@@ -17,20 +17,20 @@ public class AlgoBallIntegracionTest {
 		assertTrue(juego.obtenerPosicionDe("Goku").esIgualA(posicion));
 		juego.moverAbajo("Goku");
 		assertTrue(juego.obtenerPosicionDe("Goku").esIgualA(posicion));
-		/*el movimiento no se realizó por alli esta MajinBoo*/
+		/*el movimiento no se realizo por alli esta MajinBoo*/
 		juego.moverDerecha("Majin Boo");
 		Posicion posicionMajin = new Posicion(3,0);
 		assertTrue(juego.obtenerPosicionDe("Majin Boo").esIgualA(posicionMajin));
-		/*no se puede mover a Majin porque no es el turno del jugador dueño*/
+		/*no se puede mover a Majin porque no es el turno del jugador duenio*/
 		juego.moverIzquierda("Goku");
 		assertTrue(juego.obtenerPosicionDe("Goku").esIgualA(posicion));
-		/* el movimiento no se realizó porque alli termina el tablero*/
+		/* el movimiento no se realizo porque alli termina el tablero*/
 		juego.moverDerecha("Goku");
 		Posicion posicion2 = new Posicion(2,1);
 		assertTrue(juego.obtenerPosicionDe("Goku").esIgualA(posicion2));
 		juego.moverDerecha("Goku");
 		assertTrue(juego.obtenerPosicionDe("Goku").esIgualA(posicion2));
-		/*el movimiento no se realizó porque goku ya ha realizado la cantidad de movimientos maxima
+		/*el movimiento no se realizo porque goku ya ha realizado la cantidad de movimientos maxima
 		 * segun su velocidad*/
 		juego.finalizarTurno();
 		juego.moverArriba("Majin Boo");
