ca30e6be12f8aaf3a4295f8719d1b4ea4246aedf
matiaslebaz <mati.lebaz@gmail.com>
2017-06-11 15:40:44 -0300

    agregue lo de freezer que me había olvidado una linea

--- src/funcionamientoPersonaje/Freezer.java
+++ src/funcionamientoPersonaje/Freezer.java
@@ -13,7 +13,7 @@ public class Freezer extends Personaje
 		this.ataqueEspecial = new AtaqueEspecial(NOMBRE_ATQ_ESPECIAL_FREEZER, KI_ATQ_ESPECIAL_FREEZER);
 		this.estadoTransformacionActual = setEstadoNormal();
 		this.movimientosRestantes = estadoTransformacionActual.getVelocidad();
-
+		this.ataqueEspecial.setPorcentaje(PROCENTAJE_DANIO_ATAQ_ESPECIAL_FREEZER);
 	}
 	
 	
