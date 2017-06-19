13146742faccc9d6d92c8509d898d02dacc027cf
Bautista Canavese <bauti-canavese@hotmail.com>
2017-06-19 04:12:14 -0300

    Los mensajes por consola al usuario se muestran por tres segundos.

--- src/vistas/Consola.java
+++ src/vistas/Consola.java
@@ -1,5 +1,7 @@
 package vistas;
 
+import javafx.util.Duration;
+import javafx.animation.PauseTransition;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -7,34 +9,45 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-public class Consola extends VBox{
+public class Consola extends VBox
+{
 	private Label nombre = new Label();
+	private Label ioStream = new Label();
 	
 	public Consola(){
-		
 		nombre.setText("consola...");
 		nombre.setFont(Font.font("courier new", FontWeight.SEMI_BOLD, 14));
 		nombre.setTextFill(Color.WHITE);
 
-		
 		this.setSpacing(10);
 		this.setPadding(new Insets(15));
 		this.setStyle("-fx-background-color: black;");
-		
 		this.getChildren().add(nombre);
-		
 	}
-	public void agregarInformacion(String stg){
+	
+	public void agregarInformacion(String mensajeTemporal){
 		this.getChildren().clear();
-		Label ioStream = new Label();
-		ioStream.setText(stg);
-		ioStream.setFont(Font.font("courier new", FontWeight.SEMI_BOLD, 14));
-		ioStream.setTextFill(Color.WHITE);
-		this.getChildren().add(ioStream);
+		this.setMensajeTemporal(mensajeTemporal);
+		this.limpiarMensajeTemporal();
 	}
+	
 	public void reiniciar(){
 		this.getChildren().clear();
 		this.getChildren().add(this.nombre);
-		
+	}
+	
+	private void limpiarMensajeTemporal()
+	{
+		PauseTransition pause = new PauseTransition(Duration.seconds(3));
+		pause.setOnFinished(event -> ioStream.setText(""));
+		pause.play();
+	}
+	
+	private void setMensajeTemporal(String mensaje)
+	{
+		ioStream.setText(mensaje);
+		ioStream.setFont(Font.font("courier new", FontWeight.SEMI_BOLD, 14));
+		ioStream.setTextFill(Color.WHITE);
+		this.getChildren().add(ioStream);
 	}
 }
